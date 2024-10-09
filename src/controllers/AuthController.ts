f01f62746ad8d7bac846cc951388151e98497d5a
Mattéo Houillon <maatteoo.h@gmail.com>
2024-10-09 14:47:23 +0200

    add validation schema

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -2,9 +2,18 @@ import { Request, Response, NextFunction } from 'express';
 import User from '../models/User';
 import { comparePassword } from '../utils/passwordUtils';
 import { generateToken } from '../utils/tokenUtils';
+import { registerSchema, loginSchema } from '../models/ValidationSchema';
 
-export const register = async (req: Request, res: Response, next: NextFunction) => {
+export const register = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     const { email, password, role } = req.body;
+
+
+    const { error } = registerSchema.validate({ email, password, role });
+    if (error) {
+        res.status(400).send(error.details[0].message);
+        return;
+    }
+
     try {
         const user = new User({ email, password, role });
         await user.save();
@@ -14,8 +23,15 @@ export const register = async (req: Request, res: Response, next: NextFunction) 
     }
 };
 
-export const login = async (req: Request, res: Response, next: NextFunction) => {
+export const login = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     const { email, password } = req.body;
+
+    const { error } = loginSchema.validate({ email, password });
+    if (error) {
+        res.status(400).send(error.details[0].message);
+        return;
+    }
+
     try {
         const user = await User.findOne({ email }) as { _id: string, password: string };
         if (!user) {
