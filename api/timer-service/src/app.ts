b7a41ad1d992a2acaa2255e64999370967049bda
Mattéo Houillon <maatteoo.h@gmail.com>
2024-10-11 11:44:03 +0200

    add user and timer connection with mongoose

--- api/timer-service/src/app.ts
+++ api/timer-service/src/app.ts
@@ -1,1 +1,14 @@
-console.log('Hello from timer-service');
+import express, { Request, Response } from 'express';
+import { connectDatabase } from './controller/database.controller';
+
+const app = express();
+
+app.use(express.json());
+
+connectDatabase();
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World !');
+});
+
+export default app;
--- api/user-service/src/app.ts
+++ api/user-service/src/app.ts
@@ -1,1 +1,15 @@
-console.log('Hello from user-service');
+// src/app.ts
+import express, { Request, Response } from 'express';
+import { connectDatabase } from './controller/database.controller';
+
+const app = express();
+
+app.use(express.json());
+
+connectDatabase();
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World !');
+});
+
+export default app;
