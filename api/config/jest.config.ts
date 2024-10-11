857225f583e0c8af3200c4b1daa95eb255e1c540
Mattéo Houillon <maatteoo.h@gmail.com>
2024-10-11 11:22:07 +0200

    structure microservice

--- api/config/jest.config.ts
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  preset: 'ts-jest',
-  collectCoverage: true,
-  coverageDirectory: "coverage",
-  coverageReporters: ["json", "lcov", "text", "clover"],
-  testEnvironment: 'node',
-  testMatch: ['**/?(*.)+(spec|test).[tj]s?(x)'],
-  testPathIgnorePatterns: ['/node_modules/'],
-};
---
+++ api/gateway-service/src/app.ts
@@ -0,0 +1,1 @@
+console.log('Hello from gateway-service');
